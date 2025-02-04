2dd8ec45cc25ba6f112dda7140008aeaa2fba367
Sashi <sashi.bhusan.yadav@stud.th-luebeck.de>
2025-02-04 09:24:04 +0100

    implemented the city class for all the cities

--- src/City.java
+++ src/City.java
@@ -1,2 +1,24 @@
 public class City {
+    private String name;
+    private int heuristic;
+    private String availablePermit;
+
+    public City(String name, int heuristic, String availablePermit) {
+        this.name = name;
+        this.heuristic = heuristic;
+        this.availablePermit = availablePermit.equals("NONE") ? null : availablePermit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeuristic() {
+        return heuristic;
+    }
+
+    public String getAvailablePermit() {
+        return availablePermit;
+    }
 }
+
